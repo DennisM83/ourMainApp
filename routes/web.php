2a688d45d7eb849b2d65585b36e364652a0cdd5f
Dennis Wenrich <dennisfinley@gmail.com>
2023-04-30 22:19:01 -0400

    created route for post creation

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\ExampleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +15,13 @@ use App\Http\Controllers\ExampleController;
 |
 */
 
+
+// User related routes
 Route::get('/', [UserController::class, "showCorrectHomePage"]);
 Route::post('/register', [UserController::class, "register"]);
 Route::post('/login', [UserController::class, "login"]);
 Route::post('/logout', [UserController::class, "logout"]);
+
+
+//Post related routes
+Route::get('/create-post', [PostController::class, 'showCreateForm']);
