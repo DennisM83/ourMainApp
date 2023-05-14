6a21cb2be45e34e1c0442fc88f07b04a30f27819
Dennis Wenrich <dennisfinley@gmail.com>
2023-05-14 10:14:00 -0400

    added markdown feature

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function viewSinglePost(Post $post) {
+        $post['body'] = strip_tags(Str::markdown($post->body), '<p><br><strong><h3><ul><ol><li><h2>');
         return view('single-post', ['post' => $post]);
     }
 
